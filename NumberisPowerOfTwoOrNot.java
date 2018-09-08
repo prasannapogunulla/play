edaeab12914c3e70d16b94479832688b6698f947
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-08 16:01:51 +0530

    Create NumberisPowerOfTwoOrNot.java

---
+++ NumberisPowerOfTwoOrNot.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class NumberisPowerOfTwoOrNot
+{
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int number = in.nextInt();
+    in.close();
+
+    if (isPowerOfTwo(number)) {
+      System.out.println("yes");
+    } else {
+      System.out.printf("no");
+    }
+  }
+
+  private static boolean isPowerOfTwo(int number) {
+
+    if (number % 2 != 0) {
+      return false;
+    } else {
+
+      for (int i = 0; i <= number; i++) {
+
+        if (Math.pow(2, i) == number) return true;
+      }
+    }
+    return false;
+  }
+}
