112a80a6aad0f5f51c493bbc9f507fac50e5a1ca
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-15 10:06:16 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+class Power
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int r=sc.nextInt();
+System.out.println((int)Math.pow(n,r)); 
+}
+}
